651f8326abd550c0444133b843baff42cb5de5f0
ilyaderezovskiy <71149562+ilyaderezovskiy@users.noreply.github.com>
2021-01-24 19:55:41 +0500

    .

---
+++ 03 module/01 seminar/home work/HW_Task3/delegate_hw/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+// Задание 3
+delegate double delegateConvertTemperature(double x);
+
+class TemperatureConverterImp
+{
+    public double ToCelsius(double temp) => 5 / 9 * (temp - 32);
+    public double ToFahrenheit(double temp) => 1.8 * temp + 32;
+}
+
+class StaticTempConverters
+{
+    public static double ToKelvin(double temp) => temp + 273.15;
+    public static double ToReaumur(double temp) => 0.8 * temp;
+    public static double ToRankin(double temp) => (temp + 273.15) * 1.8;
+
+    public static double FromKelvin(double temp) => temp - 273.15;
+    public static double FromReaumur(double temp) => temp * 5 / 4;
+    public static double FromRankin(double temp) => (temp - 491.67) * 5 / 9;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TemperatureConverterImp temperatureConverter = new TemperatureConverterImp();
+
+        delegateConvertTemperature[] converter = new delegateConvertTemperature[]
+        {temperatureConverter.ToFahrenheit, StaticTempConverters.ToKelvin, StaticTempConverters.ToReaumur, StaticTempConverters.ToRankin};
+
+        //delegateConvertTemperature converterToCelsius = new delegateConvertTemperature(temperatureConverter.ToCelsius);
+        //delegateConvertTemperature converterToFahrenheit = new delegateConvertTemperature(temperatureConverter.ToFahrenheit);
+
+        string[] names = new string[] { "Fahrenheit ", "Kelvin ", "Reaumur ", "Rankin " };
+
+        Console.WriteLine("Введите вещественное число - температуру по шкале Цельсия:");
+        try
+        {
+            double temperature = double.Parse(Console.ReadLine());
+            for (int i = 0; i < converter.Length; i++)
+            {
+                Console.WriteLine(names[i] + Math.Round(converter[i](temperature), 3));
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Incorrect input!", e);
+        }
+    }
+}
---
+++ 03 module/02 seminar/class work/sem_20.01/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+public class Plant
+{
+    private double growth;
+    private double photosensitivity;
+    private double frostresistance;
+
+    public Plant(double gr, double photo, double frost)
+    {
+        growth = gr;
+        photosensitivity = photo;
+        frostresistance = frost;
+    }
+
+    public double Growth
+    {
+        get
+        {
+            return growth;
+        }
+
+        set
+        {
+            growth = value;
+        }
+    }
+
+    public double Photosensitivity
+    {
+        get
+        {
+            return photosensitivity;
+        }
+
+        set
+        {
+            photosensitivity = value;
+        }
+    }
+
+    public double Frostresistance
+    {
+        get
+        {
+            return frostresistance;
+        }
+
+        set
+        {
+            frostresistance = value;
+        }
+    }
+    public override string ToString() => $"Growth: {growth} Photosensitivity: {photosensitivity} Frostresistance: {frostresistance}";
+}
+class Program
+{
+    static int Sorting(Plant x, Plant y)
+    {
+        if ((int)x.Photosensitivity % 2 == 0 && (int)y.Photosensitivity % 2 != 0)
+            return -1;
+        if ((int)x.Photosensitivity % 2 != 0 && (int)y.Photosensitivity % 2 == 0)
+            return 1;
+        return 0;
+    }
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Plant[] arr = new Plant[n];
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++)
+            arr[i] = new Plant(rnd.Next(25, 101), rnd.Next(0, 101), rnd.Next(0, 101));
+        Array.ForEach(arr, a => Console.WriteLine(a));
+        Console.WriteLine();
+
+        // сортировка по возрастания роста
+        Array.Sort(arr, delegate (Plant x, Plant y)
+        {
+            if (x.Growth > y.Growth) return -1;
+            if (x.Growth < y.Growth) return 1;
+            return 0;
+        });
+        Array.ForEach(arr, a => Console.WriteLine(a));
+        Console.WriteLine();
+
+        // сортировка по убыванию морозоустойчивости
+        Array.Sort(arr, (x, y) => 
+        {
+            if (x.Frostresistance < y.Frostresistance) return -1;
+            if (x.Frostresistance > y.Frostresistance) return 1;
+            return 0;
+        });
+        Array.ForEach(arr, a => Console.WriteLine(a));
+        Console.WriteLine();
+
+        // сортировка по чётности светочувствительности
+        Array.Sort(arr, (x, y) => Sorting(x, y));
+        Array.ForEach(arr, a => Console.WriteLine(a));
+        Console.WriteLine();
+
+        Array.ConvertAll(arr, x => {
+            int frost = (int)x.Frostresistance;
+            if (frost % 2 == 0)
+                x.Frostresistance = Math.Round(x.Frostresistance / 3, 3);
+            else
+                x.Frostresistance = Math.Round(x.Frostresistance / 2, 3);
+            return x;
+        });
+        Array.ForEach(arr, a => Console.WriteLine(a));
+    }
+}
